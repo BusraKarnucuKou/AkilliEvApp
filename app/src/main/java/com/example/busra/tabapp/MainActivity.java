7900751dbe8ddcf51218d72b34dc216be7144774
BusraKarnucuKou <130201076@kocaeli.edu.tr>
2017-03-21 02:25:25 -0700

    resimler değişti

--- app/src/main/java/com/example/busra/tabapp/MainActivity.java
+++ app/src/main/java/com/example/busra/tabapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by busra on 18.03.2017.
@@ -13,6 +14,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btn_giris;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_container);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         btn_giris = (Button) findViewById(R.id.btn_giris);
 
